f9911867cdb227ffb5c2d524a18830a41c7ff505
Lunox <lunoxx.dev@gmail.com>
2025-07-02 09:24:52 +0530

    Migrations

---
+++ prisma/migrations/20250702034847_mobile_added_for_user/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[mobile]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "mobile" TEXT;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_mobile_key" ON "User"("mobile");
